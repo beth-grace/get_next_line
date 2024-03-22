12bf9292a5923451522d7cda275e95fa5aa9d5a1
Bethany Milford <bmilford@F3C014.42adel.org.au>
2024-03-22 12:33:30 +1030

    tried gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,28 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:34:45 by bmilford          #+#    #+#             */
-/*   Updated: 2024/03/20 18:41:22 by bmilford         ###   ########.fr       */
+/*   Updated: 2024/03/22 12:32:35 by bmilford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
+	static int	*len
+	int			index
+	static int	*buf
+
+	index = 0;
+	len = BUFFER_SIZE;
+	while (buf)
+	{
+		buf = malloc(BUFFER_SIZE + 1) * (sizeof(int));
+		read (fd, buf, BUFFER_SIZE)
+			while(buf != '\n')
+			{
+				if (buf[index] == len)
+					size = malloc(BUFFER_SIZE + size) * (sizeof(int));
+				index++;
+			}
+		 (
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:42:02 by bmilford          #+#    #+#             */
-/*   Updated: 2024/03/20 18:44:41 by bmilford         ###   ########.fr       */
+/*   Updated: 2024/03/22 12:04:09 by bmilford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFFER_SIZE
 char	*get_next_line(int fd);
 
 #endif
